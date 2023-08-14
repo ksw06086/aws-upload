e41664653521b838d5887d80ea3a4f6bed1d167b
unknown <k-sunwo@naver.com>
2023-08-14 20:51:23 +0900

    s3 version @3 update and code update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sharp = require('sharp'); // 이미지 리사이징 라이브러리
-const { S3Client } = require('@aws-sdk/client-s3');
+const { S3Client, GetObjectCommand, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 // 액세스키 따로 안넣는 건 람다가 실행되면 알아서 키를 넣어주어서
 const s3 = new S3Client(); 
@@ -13,17 +13,23 @@ exports.handler = async (event, context, callback) => {
     console.log('name', filename, 'ext', ext);
 
     try {
-        const s3Object = await s3.getObject({ Bucket, Key });
+        const getObject = await s3.send(new GetObjectCommand({ Bucket, Key }));
+        // 스트림을 버퍼로 변경
+        const buffers = [];
+        for await (const data of getObject.Body){
+            buffers.push(data);
+        }
+        const imageBuffer = Buffer.concat(buffers);
         console.log('original', s3Object.Body.length);
         const resizedImage = await sharp(s3Object.Body)
                 .resize(200, 200, { fit: 'inside' })
                 .toFormat(requiredFormat)
                 .toBuffer();
-        await s3.putObject({
+        await s3.send(new PutObjectCommand({
             Bucket,
             Key: `thunb/${filename}`, // thunb/고양이.png
             Body: resizedImage,
-        })
+        }));
         console.log('put', resizedImage.length);
         return callback(null, `thunb/${filename}`); // passport의 done의 cb(callback) 생각하면 됨
     } catch (error) {
